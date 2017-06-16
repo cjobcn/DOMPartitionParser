cb8bda7e53bbd2ec5c56744a79d4f31b8fe5d457
DFFuture <albert.zhang23@gmail.com>
2017-06-16 11:25:00 +0800

    查看to_support

--- application/index/controller/FileHandler.php
+++ application/index/controller/FileHandler.php
@@ -8,12 +8,32 @@
 
 namespace app\index\controller;
 use app\index\Parser\ParserLog;
+use app\index\Parser\ResumeParser;
 use think\Controller;
 
 class FileHandler extends Controller {
 
+
+    /**对文件分类
+     * @param $dir
+     */
     public function classify($dir) {
         //$dir = "E:/PHP/wamp64/www/parser/resumes/14/";
         ParserLog::classify($dir);
+    }
+
+    public function resume($dir, $id, $template = '') {
+        $content = $this->getResume($dir, $id, $template);
+        echo $content;
+    }
+
+    public function getResume($dir, $id, $template = '') {
+        $Parser = new ResumeParser();
+        $path = ParserLog::LOG_DIR.$dir.'/'.$template.'/'.$id.'.html';
+        //dump($path);
+        $content = $Parser->readDocument($path);
+        //dump($content);
+        $content = $Parser->convert2UTF8($content);
+        return $content;
     }
 }
