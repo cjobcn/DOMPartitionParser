b7093dd3644688e555630abc928b034519ba4dd3
sjy7874 <sjy7874@qq.com>
2017-09-30 11:57:10 +0800

    修改了数据库密码

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'localhost',
+    'hostname'        => '192.168.0.250',
     // 数据库名
     'database'        => 'jianlibao',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '123',
+    'password'        => 'G*^FH7gK%^YE(O*TKGUD%*^D',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
