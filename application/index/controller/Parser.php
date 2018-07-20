de475e61ddb1332440f55a017a905090ee20c9ac
Royhc <1195089759@qq.com>
2018-07-20 16:00:05 +0800

    111

--- application/index/controller/Parser.php
+++ application/index/controller/Parser.php
@@ -19,16 +19,16 @@ class Parser extends Controller {
     //简历解析
     public function resume() {
         header('Access-Control-Allow-Origin:*');
-        $dir = dirname(__FILE__);
-        $path = $dir.'/lc1.txt';
-        $originContent = file_get_contents($path);
-        $isRecommendReport = true;
-        if(1){
-//        $request = request();
-//        if($request->isPost()) {
-//            $originContent = $request->post('content');
-//            $id = $request->post('id');
-//            $isRecommendReport = $request->post('isRecommendReport')?:false;//是否是推荐报告
+//        $dir = dirname(__FILE__);
+//        $path = $dir.'/lc1.txt';
+//        $originContent = file_get_contents($path);
+//        $isRecommendReport = true;
+//        if(1){
+        $request = request();
+        if($request->isPost()) {
+            $originContent = $request->post('content');
+            $id = $request->post('id');
+            $isRecommendReport = $request->post('isRecommendReport')?:false;//是否是推荐报告
             $originContent = preg_replace('/(?<!\r)\n/',"\r\n",$originContent);
             //内容丢失
             if(!$originContent || !is_string($originContent))
