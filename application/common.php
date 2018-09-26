fb16be8319f8afca3e81e559bb69c2ae2aa02bb6
Royhc <1195089759@qq.com>
2018-09-26 16:17:26 +0800

    20

--- application/common.php
+++ application/common.php
@@ -87,7 +87,6 @@ function delData(&$data){
 //学校名= 中学 || 高 || 中 结尾 + major 含有 ”高中“
 function dealEducation(&$data){
     if($data['education']){
-        vde($data['education']);
         sort_arr_by_field($data['education'],'start_time',true);
         $blackName = '/高中$|中学$|高$|中$/';
         foreach($data['education'] as $key=>$value){
